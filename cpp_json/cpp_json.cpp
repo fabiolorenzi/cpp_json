9127709360727eb1e142ed5cfabce74f68917a51
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-01-26 11:07:16 +0000

    first commit

---
+++ cpp_json/cpp_json.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <json/value.h>
+#include <json/json.h>
+
+int main()
+{
+    std::ifstream file("test.json");
+    Json::Value actualJson;
+    Json::Reader reader;
+
+    reader.parse(file, actualJson);
+
+    // The code below is to get all the json
+    //std::cout << actualJson << std::endl;
+
+    // The code below is to get a specific key from the json
+    std::cout << "Name: " << actualJson["name"] << std::endl;
+
+    return 0;
+}
